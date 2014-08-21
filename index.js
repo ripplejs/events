493d4c65be1cff31a11dd44e104cc129a5693771
bmcmahen <ben.mcmahen@gmail.com>
2014-08-21 11:26:09 -0600

    change api to accept arguments in-line with other plugins

--- index.js
+++ index.js
@@ -16,19 +16,21 @@ var events = [
   'keyup'
 ];
 
-module.exports = function(View) {
-  events.forEach(function(name){
-    View.directive('on-' + name, {
-      update: function(fn){
-        if(this.callback) {
+module.exports = function() {
+  return function (View) {
+    events.forEach(function(name){
+      View.directive('on-' + name, {
+        update: function(fn){
+          if(this.callback) {
+            this.node.removeEventListener(name, this.callback, true);
+          }
+          this.callback = fn.bind(this.view);
+          this.node.addEventListener(name, this.callback, true);
+        },
+        unbind: function(){
           this.node.removeEventListener(name, this.callback, true);
         }
-        this.callback = fn.bind(this.view);
-        this.node.addEventListener(name, this.callback, true);
-      },
-      unbind: function(){
-        this.node.removeEventListener(name, this.callback, true);
-      }
+      });
     });
-  });
+  };
 };
